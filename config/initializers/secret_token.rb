9d5639fd79166053b6022b735cec16bd5290bf54
MunkiPhD <munkiphd@gmail.com>
2012-05-01 12:39:19 -0400

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+League::Application.config.secret_token = '672017a98cea25188aca1bbe0cb9806013c9e02548feed4156f3d310a757194794aae8a2c61fbc0fa92faa5446f9415695df1bc0d9017bf17311bb14cef150bd'
